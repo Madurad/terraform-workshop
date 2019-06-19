5e97e8d1ff2c891937f4dd3b11a0920216dd8782
Madura Dissanayake <maduradissanayake@Madura-MacBook-Pro.local>
2019-06-19 12:59:49 +0530

    updated JAVA install

--- Jenkins-install/files/jenkins-install.sh
+++ Jenkins-install/files/jenkins-install.sh
@@ -2,7 +2,8 @@
 
 cd /tmp
 mkdir sample-directory
-sudo yum install java-1.8.0 -y
+#sudo yum install java-1.8.0 -y
+sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
 
 # Installing Jenkins Latest version
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
@@ -17,7 +18,7 @@ sudo yum install git -y
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
-
+sudo chkconfig docker on
 
 #cd /opt/
 #wget localhost:8080/jnlpJars/jenkins-cli.jar
