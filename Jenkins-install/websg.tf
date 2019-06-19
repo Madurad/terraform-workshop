b269a5ac0fd9e828727cfe209e0c71aa66cf2beb
Madura Dissanayake <maduradissanayake@Madura-MacBook-Pro.local>
2019-06-19 22:19:59 +0530

    chnaged SG

--- Jenkins-install/websg.tf
+++ Jenkins-install/websg.tf
@@ -21,14 +21,14 @@ resource "aws_security_group" "vpc-web-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["203.115.28.100/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["203.115.28.100/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
