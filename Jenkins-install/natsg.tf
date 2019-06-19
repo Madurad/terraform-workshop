c75ea5218c8106b6edb6a9dfaa6c09ec44dd3755
Madura Dissanayake <maduradissanayake@Madura-MacBook-Pro.local>
2019-06-19 11:54:05 +0530

    udated SGs

--- Jenkins-install/natsg.tf
+++ Jenkins-install/natsg.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "vpc-nat-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.115.28.100/32"]
   }
 
   ingress {
