cc5de00565adcafded223f495e16c36bf8fb3ebe
Madura Dissanayake <maduradissanayake@Madura-MacBook-Pro.local>
2019-06-19 13:19:09 +0530

    added JAVA Env. variables

--- Jenkins-install/files/jenkins-install.sh
+++ Jenkins-install/files/jenkins-install.sh
@@ -5,6 +5,13 @@ mkdir sample-directory
 #sudo yum install java-1.8.0 -y
 sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
 
+# Setup JAVA_HOME Variables
+sudo echo "JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-0.amzn2.x86_64/bin/java"" >> ~/.bashrc
+sudo echo "JRE_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-0.amzn2.x86_64/jre/bin/java"" >> ~/.bashrc
+sudo echo "PATH=$PATH:$HOME/bin:JAVA_HOME:JRE_HOME" >> ~/.bashrc
+
+sudo source ~/.bashrc
+
 # Installing Jenkins Latest version
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
