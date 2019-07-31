4d1c2d61161d4f7d8ecdd681e1053774b58dbfe2
Madura Dissanayake <maduradissanayake@Madura-MacBook-Pro.local>
2019-07-31 14:28:19 +0530

    init commit

---
+++ Kubectl Setup in Linux/files/tools-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /tmp
+mkdir sample-directory
+#sudo yum install java-1.8.0 -y
+sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
+
+# Setup JAVA_HOME Variables
+sudo echo "JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-0.amzn2.x86_64/bin/java"" >> ~/.bashrc
+sudo echo "JRE_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-0.amzn2.x86_64/jre/bin/java"" >> ~/.bashrc
+sudo echo "PATH=$PATH:$HOME/bin:JAVA_HOME:JRE_HOME" >> ~/.bashrc
+
+sudo source ~/.bashrc
+
+# Installing Jenkins Latest version
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins -y 
+sudo service jenkins start
+
+# Installing Git Client
+sudo yum install git -y
+
+# Installing Docker
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo chkconfig docker on
+
+#cd /opt/
+#wget localhost:8080/jnlpJars/jenkins-cli.jar
+
+# Install Kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
